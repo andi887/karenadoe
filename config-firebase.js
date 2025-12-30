a9b51bb234d257aaa269001bd1d7aa5dfe7a76cd
andi139 <akkoandi@gmail.com>
2025-12-30 20:53:58 +0800

    Update config-firebase.js
    Signed-off-by: andi139 <akkoandi@gmail.com>

--- config-firebase.js
+++ config-firebase.js
@@ -6,7 +6,7 @@ if (typeof firebase !== 'undefined' && !firebase.apps.length) {
   const firebaseConfig = {
     apiKey: "AIzaSyCr5B0P0X_eqMNYbZonu40NXRxqCmx4t_o",
     authDomain: "sabar-2e686.firebaseapp.com",
-    databaseURL: "https://sabar-2e686-default-rtdb.asia-southeast1.firebasedatabase.app/",
+    databaseURL:"https://sabar-2e686-default-rtdb.asia-southeast1.firebasedatabase.app/",
     projectId: "sabar-2e686",
     storageBucket: "sabar-2e686.firebasestorage.app",
     messagingSenderId: "917602556667",
@@ -21,4 +21,5 @@ if (typeof firebase !== 'undefined' && !firebase.apps.length) {
   if (typeof firebase.analytics !== 'undefined') {
     firebase.analytics();
   }
+
 }
